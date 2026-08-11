db80c51797d0f2274a91d2009e7a6b0c896f09d9
Chris Raible <chris@ghost.org>
2026-08-11 14:19:09 -0400

    Added Pro query parameter filtering to local development
    no ref
    
    Local requests previously bypassed Ghost(Pro)'s public query parameter allowlist, allowing new parameters to work locally but fail after deployment.

--- ghost/core/core/server/web/parent/middleware/index.js
+++ ghost/core/core/server/web/parent/middleware/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     emitEvents: require('./emit-events'),
+    filterQueryParameters: require('./filter-query-parameters'),
     ghostLocals: require('./ghost-locals'),
     logRequest: require('./log-request'),
     queueRequest: require('./queue-request'),
